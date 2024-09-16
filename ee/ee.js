2d92908e461ff8384ffb484e0cd6d8cf513c6928
vladkanmsk <vladkanmsk@icloud.com>
2024-09-16 17:41:19 +0300

    ee

---
+++ ee/ee.js
@@ -0,0 +1,13 @@
+let arr1 = []
+for (let i = 0; i <= 100; i++) {
+    arr1.push(i);
+    const element = arr1 [i]
+
+}
+
+console.log(arr1);
+
+let sum=0
+for (let i=0; i < arr1.length; i++){
+    sum = sum + arr1 [i]
+}

b1a7fac91be9fd65d9bdc1403c171dca83c62148
vladkanmsk <vladkanmsk@icloud.com>
2024-09-25 10:59:30 +0300

    чо

---
+++ gdz/gdz.js
@@ -0,0 +1,12 @@
+
+const newaarray = []
+
+let a = prompt('123456')
+newaarray.push(a)
+let b = prompt('qwerty')
+newaarray.push(b)
+let v = prompt('bebra') 
+
+newaarray.push(v)
+console.log(newaarray.includes('slovo'))
+console.log(newaarray)

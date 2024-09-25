2376339f2b12677950a31db95b9f5290fb8b3309
KrutelykyTeam <>
2024-09-25 17:35:32 +0300

    VESLO

---
+++ veselo/veslo.js
@@ -0,0 +1,10 @@
+const alphabet = ["А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я"]
+const alphabetBox = document.querySelector('.alphabet_box');
+for (let i = 0; i< alphabet.length; i++) {
+    const bukva = alphabet[i];
+    console.log(bukva)
+    const div = document.createElement ("div");
+alphabetBox.appendChild(div);
+div.classList.add('alphabetLetter')
+div.innerHTML  = alphabet[i];
+}

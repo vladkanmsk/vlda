e779019f3666e61218ea215754746cc4ad9c4b6d
vladkanmsk <vladkanmsk@icloud.com>
2024-09-30 17:46:07 +0300

    veslo

--- veselo/veslo.js
+++ veselo/veslo.js
@@ -7,4 +7,18 @@ for (let i = 0; i< alphabet.length; i++) {
 alphabetBox.appendChild(div);
 div.classList.add('alphabetLetter')
 div.innerHTML  = alphabet[i];
+div.addEventListener('click', () =>{
+    console.log(div)
+})
+}
+
+const wordBox = document.querySelector('.word')
+const wordArray = ["ГУСЬ", "БОБР", "НАЙКПРО", "КУРВА", "ЩОВЕЛЬ"];
+let randomindex = Math.floor( Math.random()*5)
+let WORD = wordArray [randomindex];
+for (let i = 0; i < WORD.length; i++) {
+    const div = document.createElement ("div");
+wordBox.appendChild(div);
+div.classList.add('let')
+div.innerHTML  = WORD[i];
 }

d9b6d25b79d08fd3375eb5e46e66c6888dd6c2d0
vladkanmsk <vladkanmsk@icloud.com>
2024-10-02 17:38:39 +0300

    veslo

--- veselo/veslo.js
+++ veselo/veslo.js
@@ -2,14 +2,15 @@ const alphabet = ["А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж", "З", "И", "�
 const alphabetBox = document.querySelector('.alphabet_box');
 for (let i = 0; i< alphabet.length; i++) {
     const bukva = alphabet[i];
-    console.log(bukva)
-    const div = document.createElement ("div");
-alphabetBox.appendChild(div);
-div.classList.add('alphabetLetter')
-div.innerHTML  = alphabet[i];
-div.addEventListener('click', () =>{
-    console.log(div)
-})
+//     console.log(bukva)
+//     const div = document.createElement ("div");
+// alphabetBox.appendChild(div);
+// div.classList.add('alphabetLetter')
+// div.textContent  = alphabet[i];
+creatingBlock(alphabetBox, alphabet,"alphabetLetter", i )
+//div.addEventListener('click', () =>{
+//    console.log(div)
+//})
 }
 
 const wordBox = document.querySelector('.word')
@@ -17,8 +18,16 @@ const wordArray = ["ГУСЬ", "БОБР", "НАЙКПРО", "КУРВА", "ЩО
 let randomindex = Math.floor( Math.random()*5)
 let WORD = wordArray [randomindex];
 for (let i = 0; i < WORD.length; i++) {
-    const div = document.createElement ("div");
-wordBox.appendChild(div);
-div.classList.add('let')
-div.innerHTML  = WORD[i];
+    //const div = document.createElement ("div");
+//wordBox.appendChild(div);
+//div.classList.add('let')
+//div.textContent  = WORD[i];
+creatingBlock(wordBox, WORD, "let", i)
 }
+function creatingBlock(parentBlock, slovo, newClass, i){
+    const div = document.createElement ("div");
+    parentBlock.appendChild(div);
+    div.classList.add(newClass)
+    div.textContent  = slovo[i];
+}
+creatingBlock(alphabetBox)

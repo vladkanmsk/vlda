7a8bb2ae2628426baada92ae1d9d5502ef7ca791
vladkanmsk <vladkanmsk@icloud.com>
2024-10-14 16:52:26 +0300

    uji

---
+++ google/js.js
@@ -0,0 +1,6 @@
+const button = document.querySelector ('button')
+console.log(button)
+button.addEventListener('click', function (){
+
+})
+buttonBox.classLics.toggle ('button')
---
+++ ii/js.js
@@ -0,0 +1,3 @@
+const firstArray = [1,2,3,4,5,6,7,8,9,10]
+let secondArray = ["1","2","3","4","5","6","7","8","9","10"]
+console.log(firstArray, secondArray)

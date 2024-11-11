537b7c36301d0205392ea61bdbd80ffdaf28155a
KrutelykyTeam <vladkanmsk@icloud.com>
2024-11-11 17:38:49 +0200

    DFDFD

--- game of memory/memory.js
+++ game of memory/memory.js
@@ -1,10 +1,37 @@
-const cards = document.querySelectoAll('cards')
-// quesySelector - ищет
-console.log(cards)
-// forEach - для каждого что то делает
-cards.forEach((card) => {
+const cards = document.querySelectorAll('.card');
+const gameboard = document.querySelector('.gameboard');
+// querySelectorAll - ищет
+console.log(cards);
+// forEach - что то для каждого
+cards.forEach((card)=>{
     card.addEventListener('click', ()=>{
-        card.querySelecto('.card__front').classList.toggle("active__front");
-        card.querySelecto('.card__back').classList.toggle("active__back");
-    })
-})
+        card.querySelector('.card__front').classList.toggle("active__front");
+        card.querySelector('.card__back').classList.toggle("active__back");
+    });
+});
+console.log("b" + "a" + + "a")
+
+let amount = 8 
+const wordsForCard = ["A", "B", "C", "D"]
+
+function geterateCards(amount, words ){
+const newCard = document.createElement('div')
+newCard.classList.add("card")
+gameboard.appendChild(newCard);
+
+const newCardFront = document.createElement('div')
+newCard.appendChild(newCardFront)
+newCardFront.classList.add("card__front")
+
+
+const newCardBack = document.createElement('div')
+newCard.appendChild(newCardBack)
+newCardBack.classList.add("card__back")
+newCardBack.textContent = words[0]
+
+
+newCard.addEventListener('click', ()=>{
+    newCard.querySelector('.card__front').classList.toggle("active__front");
+    newCard.querySelector('.card__back').classList.toggle("active__back");
+})}
+geterateCards(amount, wordsForCard)

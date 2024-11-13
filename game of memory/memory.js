68326ab4f01bb7c7928ce5b4ee2ebded60220ce2
KrutelykyTeam <vladkanmsk@icloud.com>
2024-11-13 17:33:51 +0200

    game

--- game of memory/memory.js
+++ game of memory/memory.js
@@ -14,24 +14,34 @@ console.log("b" + "a" + + "a")
 let amount = 8 
 const wordsForCard = ["A", "B", "C", "D"]
 
+
 function geterateCards(amount, words ){
-const newCard = document.createElement('div')
-newCard.classList.add("card")
-gameboard.appendChild(newCard);
+    for (let j = 0; j < words.length; j++) {
+        for (let i = 0; i < 2; i++) {
+            const newCard = document.createElement('div')
+            newCard.classList.add("card")
+            gameboard.appendChild(newCard);
+            
+            const newCardFront = document.createElement('div')
+            newCard.appendChild(newCardFront)
+            newCardFront.classList.add("card__front")
+            
+            
+            const newCardBack = document.createElement('div')
+            newCard.appendChild(newCardBack)
+            newCardBack.classList.add("card__back")
+            newCardBack.textContent = words[j]
+            
+            
+            newCard.addEventListener('click', ()=>{
+                newCard.querySelector('.card__front').classList.toggle("active__front");
+                newCard.querySelector('.card__back').classList.toggle("active__back");
+            })}}
+        const cards = [...gameboard.querySelectorAll(".card")]
+        cards.sort(() => Math.random()- 0.5 )
+    .map(card => gameboard.appendChild (card))
+        }
 
-const newCardFront = document.createElement('div')
-newCard.appendChild(newCardFront)
-newCardFront.classList.add("card__front")
-
-
-const newCardBack = document.createElement('div')
-newCard.appendChild(newCardBack)
-newCardBack.classList.add("card__back")
-newCardBack.textContent = words[0]
-
-
-newCard.addEventListener('click', ()=>{
-    newCard.querySelector('.card__front').classList.toggle("active__front");
-    newCard.querySelector('.card__back').classList.toggle("active__back");
-})}
 geterateCards(amount, wordsForCard)
+
+

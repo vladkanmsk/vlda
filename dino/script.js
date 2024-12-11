287e7eb3694e1ebb79033833b32c5d24e464c1b6
KrutelykyTeam <vladkanmsk@icloud.com>
2024-12-11 17:33:25 +0200

    d

--- dino/script.js
+++ dino/script.js
@@ -1,3 +1,21 @@
+const dino = document.querySelector('.dino');
+let isJumping = false;
 document.addEventListener("keydown", (e) => {
-    console.log(e.keyCode)
+    console.log(e.code)
+    jump(e.code)
 })
+
+
+function jump(key){
+    if(key == "Space" && !isJumping){
+        isJumping = true;
+console.log('jump')
+dino.style.bottom = "200px"
+setTimeout(()=>{
+    dino.style.bottom = "50px"
+}, 400)
+setTimeout(()=>{
+    isJumping = false
+}, 800)
+    }
+}   

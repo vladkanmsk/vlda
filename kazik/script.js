d13959203faf6751d500dd5ae374c4f7994e347c
vladkanmsk <vladkanmsk@icloud.com>
2025-07-29 13:54:22 +0300

    ва

--- kazik/script.js
+++ kazik/script.js
@@ -1,8 +1,12 @@
 const button = document.querySelector(".button")
 let balance = 100
-button.addEventListener("click", ()=>{
-    console.log(button)
-    const userStavka = prompt('Введите ставку')
-    balance = Math.round(balance - userStavka + userStavka*Math.random()*2)
-    alert(`Баланс: ${balance}, ставка: ${userStavka}`)
-})
+
+button.addEventListener("click", () => {
+  let userStavka;
+  do {
+    userStavka = prompt(`Введите ставку (от 1 до ${balance}):`);
+    userStavka = Number(userStavka);
+  } while (isNaN(userStavka) || userStavka <= 0 || userStavka > balance);
+  balance = Math.round(balance - userStavka + userStavka * Math.random() * 2);
+  alert(`Баланс: ${balance}, ставка: ${userStavka}`);
+});

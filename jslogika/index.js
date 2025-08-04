2c04a58ca3065462b502e46b80b29390919447b7
krutel <vladkanmsk@icloud.com>
2025-08-04 17:43:34 +0300

    dfgdf

---
+++ jslogika/index.js
@@ -0,0 +1,76 @@
+//1.1.1
+
+// let a = Math.random()-0.5
+// console.log(a)
+// if(a < 0 ){
+    // console.log("число відємне")
+// }
+
+
+//1.1.2
+
+// let str = "Hello!!"
+// console.log(str.length)
+
+// //1.1.3
+
+// let str1 = "HELLO"
+// console.log(str1.length)
+// console.log(str1[str1.lenght - 1])
+
+// //1.1.4
+
+// let a = 546
+
+// if(a % 2){
+//     console.log(1)
+// }else{
+//     console.log(0)
+// }
+
+// //1.1.5
+// let slovo1 = "Hello"
+// let slovo2 = "HELP"
+// if (slovo1[0] == slovo2[0]){
+//     console.log("ОК")
+// }
+
+//1.1.6
+
+// let str = "КАРУСЕЛЬ"
+// let str1 = "КАРУСЕЛ"
+// let lastLetter = str[str.length - 1]
+// let lastLetter1 = str1[str1.length - 1]
+// if(lastLetter == 'Ь'){
+//     console.log("мгкий знак о ноу")
+//     console.log(str[str.length - 2])
+// }else{
+//     console.log(lastLetter)
+// }
+
+//1.2.1
+
+// let a = 11
+// a = String(a)
+// console.log(a[0])
+
+//1.2.2
+
+// let a = 123
+// a = String(a)
+// console.log(a[a.length - 1])
+
+//1.2.3
+
+// let a = 12345
+// a = String(a)
+// let sum = Number(a[0]) + Number(a[a.length -1])
+// console.log(sum)
+
+//1.2.4
+
+let a = "НОВЫЙ"
+let str = a.length
+if(str > 1){
+    console.log(a[a.length - 2])
+}

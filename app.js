693909989fb551ef7dc11b890c2e28f6265e9e99
vlada <vladkanmsk@icloud.com>
2026-01-12 16:49:32 +0200

    hfghfghfg

---
+++ app.js
@@ -0,0 +1,26 @@
+const container = document.querySelector('#cards')
+function createCard({title}){
+    const card = document.createElement('div')
+    card.classList.add('card')
+
+    const h3 = document.createElement('h3')
+    h3.classList.add('card_title')
+    h3.textContent = title
+
+    
+
+    card.appendChild(h3)
+    return card
+}
+async function init() {
+    try{
+        const res = await fetch ('./data.json')
+        const data = await res.json()
+        data.items.forEach(item => {
+            container.appendChild(createCard(item))
+        });
+    } catch (error){
+        console.error(error)
+    }
+}
+init()
